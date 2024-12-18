6da3f6e666402077fca76a62e00af9c0a7fe9f66
lukacera <lukaceranic38@gmail.com>
2024-12-18 17:50:09 +0100

    Revert middleware back to the original state

--- middleware.ts
+++ middleware.ts
@@ -6,27 +6,23 @@ export default auth(async (req) => {
     const isPublicPath = publicPaths.some(path => 
         req.nextUrl.pathname.startsWith(path)
     )
-    const isApiPath = req.nextUrl.pathname.startsWith('/api');
-    console.log("d")
+
     const session = req.auth
-    console.log('Session:', session)
-    // API Protection
-    if (isApiPath && !session) {
-        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-    }
+
     if (req.nextUrl.pathname === '/login' && session) {
         return NextResponse.redirect(new URL('/', req.url))
     }
-    
+
     // Root path redirect
     if (req.nextUrl.pathname === '/') {
         return NextResponse.redirect(new URL('/home', req.url))
     }
+    
     // If on protected path and not authenticated, redirect to login
     if (!isPublicPath && !session) {
         return NextResponse.redirect(new URL('/login', req.url))
     }
- 
+
     // Only check DB for authenticated users on protected paths
     if (!isPublicPath && session) {
         try {
@@ -42,11 +38,11 @@ export default auth(async (req) => {
             return NextResponse.redirect(new URL('/login', req.url))
         }
     }
- 
+
     return NextResponse.next()
- 
+
 })
 
 export const config = {
-    matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'], // Includes `/api`
-};
+   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+}
